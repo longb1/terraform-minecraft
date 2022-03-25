db5cf66b093a36fb874ebee7dc9c131eeabbcb06
long <17272827+longb1@users.noreply.github.com>
2022-03-25 12:41:38 +0000

    Added initial networking components
    Added networking components to move the Minecraft server off default VPC
    for security and organisation purposes.

--- networking.tf
+++ networking.tf
@@ -24,3 +24,55 @@ resource "aws_security_group" "minecraft" {
     Name = "Minecraft"
   }
 }
+
+resource "aws_vpc" "mc_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "mc_vpc_subnet" {
+  vpc_id     = aws_vpc.mc_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_security_group" "mc_vpc_sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.mc_vpc.id
+
+  ingress {
+    description = "Receive SSH from home."
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.your_ip}/32"]
+  }
+  ingress {
+    description = "Receive Minecraft from everywhere."
+    from_port   = 19132
+    to_port     = 19132
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Send everywhere."
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Minecraft"
+  }
+}
+
+resource "aws_internet_gateway" "mc_vpc_gw" {
+  vpc_id = aws_vpc.mc_vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
